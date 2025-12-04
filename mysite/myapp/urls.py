06fde75d4e7bf388319fddfa074413f821c43305
Marty Carr <mcarr390@gmail.com>
2025-12-04 14:59:48 -0600

    templates

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('item/', views.item)
+    path('item/', views.item),
+    path('<int:id>/', views.detail)
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Item
 
 
 def index(request):
-    return HttpResponse("hello world")
+    item_list = Item.objects.all()
+    context = {
+        'item_list':item_list
+    }
+    return render(request, "myapp/index.html", context)
+
+def detail(request, id):
+    return HttpResponse(f'this is a detail view for item with id: {id}')
 
 def item(request):
     return HttpResponse("this is an item view")
